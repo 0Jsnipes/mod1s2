fbe3a1249b789226f62c81869bdd075d1a56ae26
0Jsnipes <jared.c.snipes@gmail.com>
2023-12-10 19:01:44 -0500

    declaring an object

--- JSnotes.js
+++ JSnotes.js
@@ -1,1 +1,11 @@
-let name = "Amanda"
+const name = "Amanda Johnson";
+const email = 'amandaj@gmail.com';
+let state = 'Florida';
+let favoriteProgrammingLanguage = 'Javascript';
+
+const  user = {
+    name: 'Amanda Johnson';
+    email: 'amandaj@gmail.com';
+    state: 'Florida';
+    'favorite programming language': 'Javascript';
+};
