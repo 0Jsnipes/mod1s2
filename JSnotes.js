341dae32807756e820473d02a365771e164848a4
0Jsnipes <jared.c.snipes@gmail.com>
2023-12-11 21:30:23 -0500

    just starting arrays

--- JSnotes.js
+++ JSnotes.js
@@ -1,11 +1,34 @@
-const name = "Amanda Johnson";
+/*const name = "Amanda Johnson";
 const email = 'amandaj@gmail.com';
 let state = 'Florida';
 let favoriteProgrammingLanguage = 'Javascript';
 
 const  user = {
-    name: 'Amanda Johnson';
-    email: 'amandaj@gmail.com';
-    state: 'Florida';
-    'favorite programming language': 'Javascript';
+    name: 'Amanda Johnson',
+    email: 'amandaj@gmail.com',
+    state: 'Florida',
+    'favorite programming language': 'Javascript',
 };
+const user2 ={
+    name: 'Josh Carter',
+    email: 'carterjosh@gmail.com',
+    state: 'Utah',
+    'favorite programing language': 'Javascript'
+};
+let nameToDisplay = user2.name;
+console.log(`Welcome Back ${nameToDisplay}!`);
+user2.state;
+user2.email;
+user2['favorite programming language'];
+*/
+const user= {
+    name: 'Fabiola Dallas',
+    email: 'fabdallas@noemail.com',
+    stat: 'Texas',
+    'favorite programming language': 'Python'
+};
+user['Github Username'] = 'Fabiloa123';
+user.state = 'TX';
+delete user['favorite programming language']
+
+const concerts =['April 26', 'April 29', 'May 5', 'May 10', 'May 17'];
